df0fdada51e828f7f6c52ebee76951bf190e7026
Adrian <adrian.kuhn@arcor.de>
2025-07-30 23:59:08 +0200

    chor: fix linter errors

--- dnd-schema-input/src/dnd/TreeContainer.tsx
+++ dnd-schema-input/src/dnd/TreeContainer.tsx
@@ -1,14 +1,16 @@
 import {TreeNode} from "./TreeNode.ts";
 
 export function TreeContainer() {
-    let root: TreeNode;
     const props = Array.from({length: 13}, (_, index) => (index + 1).toString())
     console.log(props);
-    root = TreeNode.createBinaryTree(props);
+    const root: TreeNode = TreeNode.createBinaryTree(props);
 
     return (
         <>
             <h1>hello</h1>
+            <div>
+                {root.title}
+            </div>
         </>
     )
 }
