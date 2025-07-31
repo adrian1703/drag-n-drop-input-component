e713ec1945447b80f7095306524adce91282581c
Adrian <adrian.kuhn@arcor.de>
2025-07-31 23:47:47 +0200

    refc: move dnd directory to tree for improved clarity

--- dnd-schema-input/src/main.tsx
+++ dnd-schema-input/src/main.tsx
@@ -1,7 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {TreeContainer} from "./dnd/TreeContainer.tsx";
+import {TreeContainer} from "./tree/TreeContainer.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
