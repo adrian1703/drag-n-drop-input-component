6fa21e7680abb4d0c7dcee2d666f35717d0869e0
Adrian <adrian.kuhn@arcor.de>
2025-08-01 22:39:10 +0200

    refc: remove clutter

--- dnd-schema-input/src/main.tsx
+++ dnd-schema-input/src/main.tsx
@@ -2,11 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import {TreeContainer} from "./tree/TreeContainer.tsx";
-import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
         <TreeContainer/>
     </StrictMode>,
 )
